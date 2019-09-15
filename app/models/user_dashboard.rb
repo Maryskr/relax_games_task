38e897fd6efd63c624a545f8a88422e17aa1d5a9
Mary Skr <maryskr93@gmail.com>
2019-09-15 22:07:03 +0300

    add uniqueness validation for user dashboard; show flash messages

--- app/models/user_dashboard.rb
+++ app/models/user_dashboard.rb
@@ -5,6 +5,7 @@ class UserDashboard < ActiveRecord::Base
   belongs_to :dashboard
 
   validates :user_id, :dashboard_id, presence: true
+  validates :user_id, uniqueness: { scope: :dashboard_id }
 
   delegate :title, :description, to: :dashboard, allow_nil: true
 
